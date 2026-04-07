6e50579f93b63af72ba0254c67468fddd5b42d5a
hrrs01 <haavard.hn@gmail.com>
2026-04-07 10:00:18 +0200

    flake updates, old gpg key, new claude version

--- modules/home/base/default.nix
+++ modules/home/base/default.nix
@@ -21,7 +21,7 @@ in
 
     # Temporary
     brave
-    claude-code
+    pkgs_unstable.claude-code
     slack
     mattermost-desktop
 
@@ -79,7 +79,7 @@ in
     userName = "hrrs01";
     userEmail = "haavard.hn@gmail.com";
     signing = {
-      key = "94DFCD695DD7C2F0"; # Set to your GPG key ID
+      key = "6EA457D3B78C4A50"; # Set to your GPG key ID
       signByDefault = true;
     };
   };
