1e52baeb0c10e3b1a1be44986ce2c24641790139
hrrs01 <haavard.hn@gmail.com>
2026-09-17 10:34:56 +0200

    feat: bump zed

--- packages/zed-preview/default.nix
+++ packages/zed-preview/default.nix
@@ -22,11 +22,11 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "zed-preview";
-  version = "1.19.1-pre";
+  version = "1.21.0-pre";
 
   src = fetchurl {
     url = "https://github.com/zed-industries/zed/releases/download/v${finalAttrs.version}/zed-linux-x86_64.tar.gz";
-    hash = "sha256-lWTKyMZDdg+U+MIg9Qy66VyHeFJCie2W50SvLH0J4EM=";
+    hash = "sha256-6twwCYGWoKISmIpepNGT54ZB4XtFnkJ13ddnUXVEm0Q=";
   };
 
   sourceRoot = "zed-preview.app";
